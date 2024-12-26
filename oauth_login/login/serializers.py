21d2b2aa9514c7b8d469faf5b6f987665c5c8aa8
SuneLarsson <boinkswe@gmail.com>
2024-12-26 15:52:12 +0100

    Added registration and deletion of user, as well as update of password.

--- oauth_login/login/serializers.py
+++ oauth_login/login/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
+from rest_framework.exceptions import ValidationError
 # first we define the serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,46 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ("name")
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', "first_name", "last_name")
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+    confirm_new_password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        """
+        Ensure the new password and confirm new password match.
+        """
+        if data['new_password'] != data['confirm_new_password']:
+            raise ValidationError("New passwords do not match.")
+        return data
+
+    def validate_current_password(self, value):
+        """
+        Ensure the current password is correct.
+        """
+        user = self.context.get('user')
+        if not user.check_password(value):
+            raise ValidationError("Current password is incorrect.")
+        return value
