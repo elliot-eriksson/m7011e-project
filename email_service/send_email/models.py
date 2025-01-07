7d54ad28acf989f49ff17ce29d0117847d47996d
SuneLarsson <boinkswe@gmail.com>
2025-01-07 20:19:18 +0100

    fixes to email table

--- email_service/send_email/models.py
+++ email_service/send_email/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Email(models.Model):
+    sender_id = models.IntegerField(null=True, blank=True)
+    recipient_id = models.IntegerField(null=True, blank=True)
     recipient_email = models.EmailField()
     subject = models.CharField(max_length=255)
     message = models.TextField()
     status = models.CharField(max_length=50, choices=[('SENT', 'Sent'), ('FAILED', 'Failed')])
     error_message = models.TextField(null=True, blank=True)
     sent_at = models.DateTimeField(auto_now_add=True)
-    sender_id = models.IntegerField(default=1)
-    recipient_id = models.IntegerField(default=1)
